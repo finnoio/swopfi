425fffbaab3c5394a7256fc9e34d2d70a999df2a
Dmitry Gubanov <dmitrygubanov@MacBook-Pro-Dmitry.local>
2020-10-28 18:18:11 +0300

    replanishment fixed error : long overflow

--- dApps/test.py
+++ dApps/test.py
@@ -5,39 +5,53 @@ import time
 from dAppScript import dAppScript
 import json
 
-# NODE_URL = "https://nodes-stagenet.wavesnodes.com"
+NODE_URL = "https://nodes-stagenet.wavesnodes.com"
 
-# def wait_for_resource_available(id, timeout):
-#     status_code = 0
-#     status = 0
-#     while status_code != 200:
-#         time.sleep(1)
-#         response = requests.get(NODE_URL + "/transactions/info/" + id)
-#         status_code = response.status_code
-#         try:
-#             status = json.loads(str(response.content.decode('utf8')))[
-#                 "applicationStatus"]
-#         except:
-#             status = "NotFound"
-#     return status
+def wait_for_resource_available(id, timeout):
+    status_code = 0
+    status = 0
+    while status_code != 200:
+        time.sleep(1)
+        response = requests.get(NODE_URL + "/transactions/info/" + id)
+        status_code = response.status_code
+        try:
+            status = json.loads(str(response.content.decode('utf8')))[
+                "applicationStatus"]
+        except:
+            status = "NotFound"
+    return status
 
-# script = dAppScript()
+script = dAppScript()
 
-# pw.setNode(node="https://nodes-stagenet.wavesnodes.com", chain_id="S")
+pw.setNode(node="https://nodes-stagenet.wavesnodes.com", chain_id="S")
 
-# ##if true then  throw(toString(invariantCalc(amountTokenA-amountToSendEstimated,amountTokenB + tokenReceiveAmount)) + " "+toString(invariant)) else
+##if true then  throw(toString(invariantCalc(amountTokenA-amountToSendEstimated,amountTokenB + tokenReceiveAmount)) + " "+toString(invariant)) else
     
-# test2 = pw.Address(seed = "as1sas2")
-# moneySeed = pw.Address(seed = "mutual essence merry loop margin morning involve vicious air post table faculty primary idea buffalo")
+test2 = pw.Address(seed = "as1sas22")
+moneySeed = pw.Address(seed = "mutual essence merry loop margin morning involve vicious air post table faculty primary idea buffalo")
 
-# transfer = moneySeed.sendWaves(test2,int(10**8))
-# wait_for_resource_available(transfer["id"],1000)
-# print(transfer)
+transfer = moneySeed.sendWaves(test2,int(10**8))
+wait_for_resource_available(transfer["id"],1000)
+print(transfer)
 
-# setScript = test2.setScript(script,txFee=1400000)
-# #print(setScript)
-# wait_for_resource_available(setScript["id"],1000)
+setScript = test2.setScript(script,txFee=1400000)
+#print(setScript)
+wait_for_resource_available(setScript["id"],1000)
 
+
+# fund = moneySeed.invokeScript(test2.address, "fund", [], [
+#     {"amount": int(13100000000), "assetId": "qTtranpN3eE8UDZ5kehxvHHtUggXCMTyANGv3RtvaKi" },{"amount": int(100000000), "assetId": None}], txFee=100900000)
+# print(fund)
+# statusFund = wait_for_resource_available(
+#     fund["id"], 100)
+# print(fund)
+
+replanish = moneySeed.invokeScript(test2.address, "replenishmentWithTwoToken", [{"type": "integer", "value": int(0) }], [
+    {"amount": 1310000
+, "assetId": "qTtranpN3eE8UDZ5kehxvHHtUggXCMTyANGv3RtvaKi"},{"amount": 10000, "assetId":None}], txFee=1000000)
+print(replanish)
+statusreplanish = wait_for_resource_available(
+    replanish["id"], 1000)
 
 # fund = moneySeed.invokeScript(test2.address, "fund", [], [
 #     {"amount": int(15000000000), "assetId": "qTtranpN3eE8UDZ5kehxvHHtUggXCMTyANGv3RtvaKi" },{"amount": int(327000000), "assetId": "CdNeFRKeotuA9pnS2AaEAKPUGVPT56e5FXebDuGU8XMK"}], txFee=100900000)
@@ -88,20 +102,3 @@ import json
 #     withdraw["id"], 1000)
 
 
-import pywaves as py
-
-myAddress = py.Address(seed='venture cement chimney typical assume feature nasty home sister knee section opinion control fabric soccer')
-moneySeed = py.Address(seed = "testSwopFitestSwopFitestSwopFi")
-
-transfer = moneySeed.sendWaves(myAddress,int(495000))
-
-print(transfer)
-
-# data = [{
-#         'type':'string', 
-#         'key': 'version', 
-#         'value':'1.0.0'
-#         }]
-
-# tx = myAddress.dataTransaction(data)
-# print(tx)
