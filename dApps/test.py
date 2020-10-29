21740acd668a7514d4baf1847d0fcc28f52a5293
Dmitry Gubanov <dmitrygubanov@MacBook-Pro-Dmitry.local>
2020-10-29 09:57:51 +0300

    replanish with one token commited

--- dApps/test.py
+++ dApps/test.py
@@ -27,7 +27,7 @@ pw.setNode(node="https://nodes-stagenet.wavesnodes.com", chain_id="S")
 
 ##if true then  throw(toString(invariantCalc(amountTokenA-amountToSendEstimated,amountTokenB + tokenReceiveAmount)) + " "+toString(invariant)) else
     
-test2 = pw.Address(seed = "as1sas22")
+test2 = pw.Address(seed = "as1sas22222")
 moneySeed = pw.Address(seed = "mutual essence merry loop margin morning involve vicious air post table faculty primary idea buffalo")
 
 transfer = moneySeed.sendWaves(test2,int(10**8))
@@ -39,19 +39,19 @@ setScript = test2.setScript(script,txFee=1400000)
 wait_for_resource_available(setScript["id"],1000)
 
 
-# fund = moneySeed.invokeScript(test2.address, "fund", [], [
-#     {"amount": int(13100000000), "assetId": "qTtranpN3eE8UDZ5kehxvHHtUggXCMTyANGv3RtvaKi" },{"amount": int(100000000), "assetId": None}], txFee=100900000)
-# print(fund)
-# statusFund = wait_for_resource_available(
-#     fund["id"], 100)
-# print(fund)
+fund = moneySeed.invokeScript(test2.address, "fund", [], [
+    {"amount": int(10000000000), "assetId": None},{"amount": int(327000000), "assetId":  "4zhPJWeFuPGHfQBsVctk5wqNCRoNVqGtT4YnvKJX6tVN"}], txFee=100900000)
+print(fund)
+statusFund = wait_for_resource_available(
+    fund["id"], 100)
+print(fund)
 
-replanish = moneySeed.invokeScript(test2.address, "replenishmentWithTwoToken", [{"type": "integer", "value": int(0) }], [
-    {"amount": 1310000
-, "assetId": "qTtranpN3eE8UDZ5kehxvHHtUggXCMTyANGv3RtvaKi"},{"amount": 10000, "assetId":None}], txFee=1000000)
-print(replanish)
-statusreplanish = wait_for_resource_available(
-    replanish["id"], 1000)
+# replanish = moneySeed.invokeScript(test2.address, "replenishmentWithTwoToken", [{"type": "integer", "value": int(0) }], [
+#     {"amount": 1310000
+# , "assetId": "qTtranpN3eE8UDZ5kehxvHHtUggXCMTyANGv3RtvaKi"},{"amount": 10000, "assetId":None}], txFee=1000000)
+# print(replanish)
+# statusreplanish = wait_for_resource_available(
+#     replanish["id"], 1000)
 
 # fund = moneySeed.invokeScript(test2.address, "fund", [], [
 #     {"amount": int(15000000000), "assetId": "qTtranpN3eE8UDZ5kehxvHHtUggXCMTyANGv3RtvaKi" },{"amount": int(327000000), "assetId": "CdNeFRKeotuA9pnS2AaEAKPUGVPT56e5FXebDuGU8XMK"}], txFee=100900000)
