2a1669208db7901fc5f746cf8ddd40be3e821140
Dmitry Gubanov <dmitrygubanov@MacBook-Pro-Dmitry.local>
2020-11-04 17:50:52 +0300

    tested multiple replenishment

--- dApps/test_flat.py
+++ dApps/test_flat.py
@@ -29,8 +29,8 @@ pw.setNode(node="https://nodes-stagenet.wavesnodes.com", chain_id="S")
 assetId1 = "EbgcoV8AoM7sPFav3tueLcQyLicz255Et1hZMfZxaJn2"
 assetId2 = "4zhPJWeFuPGHfQBsVctk5wqNCRoNVqGtT4YnvKJX6tVN"
 
-#test2 = pw.Address(seed = str(random.randint(1, 100000000000000000000)) + "a")
-test2 = pw.Address(seed = str("i12s22S32"))
+test2 = pw.Address(seed = str(random.randint(1, 100000000000000000000)) + "a")
+#test2 = pw.Address(seed = str("e1"))
 moneySeed = pw.Address(seed = "mutual essence merry loop margin morning involve vicious air post table faculty primary idea buffalo")
 
 
@@ -43,26 +43,26 @@ setScript = test2.setScript(script,txFee=1400000)
 wait_for_resource_available(setScript["id"],1000)
 
 fund = moneySeed.invokeScript(test2.address, "fund", [], [
-    {"amount": int(500000*1e6), "assetId": assetId1 },{"amount": int(500000*1e6), "assetId": assetId2}], txFee=100900000)
+    {"amount": int(20000*1e6), "assetId": assetId1 },{"amount": int(20000*1e6), "assetId": assetId2}], txFee=100900000)
 print(fund)
 statusFund = wait_for_resource_available(
     fund["id"], 100)
 print(fund)
 
-replanish = moneySeed.invokeScript(test2.address, "replenishmentWithTwoToken", [{"type": "integer", "value": int(10) }], [
-    {"amount": 6000000, "assetId": assetId1},
-    {"amount": 6000000, "assetId": assetId2}], txFee=1000000)
-print(replanish)
-statusreplanish = wait_for_resource_available(
-    replanish["id"], 1000)
+# replanish = moneySeed.invokeScript(test2.address, "replenishmentWithTwoToken", [{"type": "integer", "value": int(10) }], [
+#     {"amount": 6000000, "assetId": assetId1},
+#     {"amount": 6000000, "assetId": assetId2}], txFee=1000000)
+# print(replanish)
+# statusreplanish = wait_for_resource_available(
+#     replanish["id"], 1000)
 
-# invoke = moneySeed.invokeScript(test2.address, "exchanger", [{"type": "integer", "value": int(99997291) },{"type": "integer", "value":int(99997291)}], [{ "amount": 100000000, "assetId": assetId1 }], txFee=1000000)
-# print(invoke)
-# wait_for_resource_available(invoke["id"],1000)
-
-invoke = moneySeed.invokeScript(test2.address, "replanishmentWithOneToken", [{"type": "integer", "value": int(249826581) },{"type": "integer", "value":int(249823612)}], [{ "amount":  int(500000000), "assetId": assetId1 }], txFee=1000000)
+invoke = moneySeed.invokeScript(test2.address, "exchanger", [{"type": "integer", "value": int(100) },{"type": "integer", "value":int(99)}], [{ "amount": 100, "assetId": assetId1 }], txFee=1000000)
 print(invoke)
 wait_for_resource_available(invoke["id"],1000)
+
+# invoke = moneySeed.invokeScript(test2.address, "replanishmentWithOneToken", [{"type": "integer", "value": int(249826581) },{"type": "integer", "value":int(249823612)}], [{ "amount":  int(500000000), "assetId": assetId1 }], txFee=1000000)
+# print(invoke)
+# wait_for_resource_available(invoke["id"],1000)
 
 # invoke = moneySeed.invokeScript(test2.address, "exchanger", [{"type": "integer", "value": int(99997291) },{"type": "integer", "value":int(99997291)}], [{ "amount": 100000000, "assetId": assetId1 }], txFee=1000000)
 # print(invoke)
